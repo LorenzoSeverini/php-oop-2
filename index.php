9c946865d68bb25bf2f2ee566e9d58863ff33bc8
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-06-21 18:07:17 +0200

    add index.php with all import and products 😺

--- index.php
+++ index.php
@@ -1,5 +1,195 @@
 <?php
 
+// import classes
+require_once 'models/product.php';
+require_once 'models/category.php';
+require_once 'models/food.php';
+require_once 'models/toy.php';
+require_once 'models/bed.php';
+
+// product
+$product1 = new Food(
+    "Croquettes",
+    10,
+    $category1,
+    "Food",
+    '<i class="fas fa-drumstick-bite"></i>',
+);
+
+$product2 = new Toy(
+    "Ball",
+    5,
+    $category2,
+    "Toy",
+    '<i class="fas fa-futbol"></i>'
+
+);
+
+$product3 = new Bed(
+    "Bed",
+    50,
+    $category1,
+    "Bed",
+    '<i class="fas fa-bed"></i>',
+
+);
+
+$product4 = new Food(
+    "Croquettes",
+    10,
+    $category1,
+    "Food",
+    '<i class="fas fa-drumstick-bite"></i>',
+
+);
+
+$product5 = new Toy(
+    "Ball",
+    5,
+    $category2,
+    "Toy",
+    '<i class="fas fa-futbol"></i>',
+
+);
+
+$product6 = new Bed(
+    "Bed",
+    50,
+    $category1,
+    "Bed",
+    '<i class="fas fa-bed"></i>',
+
+);
+
+$product7 = new Food(
+    "Croquettes",
+    10,
+    $category1,
+    "Food",
+    '<i class="fas fa-drumstick-bite"></i>',
+
+);
+
+$product8 = new Toy(
+    "Ball",
+    5,
+    $category2,
+    "Toy",
+    '<i class="fas fa-futbol"></i>',
+
+);
+
+$product9 = new Bed(
+    "Bed",
+    50,
+    $category1,
+    "Bed",
+    '<i class="fas fa-bed"></i>',
+
+);
+
+$product10 = new Food(
+    "Croquettes",
+    10,
+    $category1,
+    "Food",
+    '<i class="fas fa-drumstick-bite"></i>',
+);
+
+$product11 = new Toy(
+    "Ball",
+    5,
+    $category2,
+    "Toy",
+    '<i class="fas fa-futbol"></i>',
+);
+
+$product12 = new Bed(
+    "Bed",
+    50,
+    $category1,
+    "Bed",
+    '<i class="fas fa-bed"></i>',
+
+);
+
+$product13 = new Food(
+    "Croquettes",
+    10,
+    $category1,
+    "Food",
+    '<i class="fas fa-drumstick-bite"></i>',
+
+);
+
+$product14 = new Toy(
+    "Ball",
+    5,
+    $category2,
+    "Toy",
+    '<i class="fas fa-futbol"></i>',
+
+);
+
+$product15 = new Bed(
+    "Bed",
+    50,
+    $category1,
+    "Bed",
+    '<i class="fas fa-bed"></i>',
+
+);
+
+$product16 = new Food(
+    "Croquettes",
+    10,
+    $category1,
+    "Food",
+    '<i class="fas fa-drumstick-bite"></i>',
+
+);
+
+// array of products
+$products = [
+    $product1,
+    $product2,
+    $product3,
+    $product4,
+    $product5,
+    $product6,
+    $product7,
+    $product8,
+    $product9,
+    $product10,
+    $product11,
+    $product12,
+    $product13,
+    $product14,
+    $product15,
+    $product16,
+];
+
+// category
+$category1 = new Category("For Dogs", '<i class="fas fa-dog"></i>');
+$category2 = new Category("For Cats", '<i class="fas fa-cat"></i>');
+
+// category for products
+$product1->category = $category1;
+$product2->category = $category2;
+$product3->category = $category1;
+$product4->category = $category1;
+$product5->category = $category2;
+$product6->category = $category1;
+$product7->category = $category1;
+$product8->category = $category2;
+$product9->category = $category1;
+$product10->category = $category1;
+$product11->category = $category2;
+$product12->category = $category1;
+$product13->category = $category1;
+$product14->category = $category2;
+$product15->category = $category1;
+$product16->category = $category1;
 
 ?>
 
@@ -24,7 +214,50 @@
 
 <body>
 
-    <h1 class="text-center p-2"><?php echo "hello world" ?></h1>
+    <!-- heder for animal shop  -->
+    <header>
+        <div class="container py-4">
+            <div class="row">
+                <div class="col-12 text-center">
+                    <h1>Animal Shop</h1>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- main for animal shop  -->
+    <main>
+        <div class="container py-4">
+            <div class="row">
+                <?php foreach ($products as $product) { ?>
+                    <div class="col-md-4">
+                        <div class="card mb-4">
+                            <div class="card-header d-flex align-items-center gap-3">
+                                <h3><?= $product->getName(); ?></h3>
+                                <div><?php echo $product->icon; ?></div>
+                            </div>
+                            <div class="card-body">
+                                <h4>Price <?= $product->getPrice(); ?> €</h4>
+                                <h5>Category: <?= $product->category->getName(); ?></h5>
+                                <div class="mb-3"><?= $product->category->icon; ?></div>
+                            </div>
+                        </div>
+                    </div>
+                <?php } ?>
+            </div>
+        </div>
+    </main>
+
+    <!-- footer for animal shop  -->
+    <footer>
+        <div class="container py-4">
+            <div class="row">
+                <div class="col-12 text-center">
+                    <h3><span><i class="fas fa-dog"></i></span> Animal Shop <span><i class="fas fa-cat"></i></span></h3>
+                </div>
+            </div>
+        </div>
+    </footer>
 
     <!-- bootstrap js -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
