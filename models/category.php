a59d5036ff5c6c802f3102d14df3a4e8ac4d8a78
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-06-21 18:05:16 +0200

    add category

---
+++ models/category.php
@@ -0,0 +1,18 @@
+<?php
+
+class Category
+{
+    public $name;
+    public $icon;
+
+    public function __construct($name, $icon)
+    {
+        $this->name = $name;
+        $this->icon = $icon;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+}
