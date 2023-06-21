4f475725c5f395ebc93fe383aca71df7243db2e6
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-06-21 18:05:31 +0200

    add bed for animal

---
+++ models/bed.php
@@ -0,0 +1,9 @@
+<?php
+
+class Bed extends Product
+{
+    public function __construct($name, $price, $category, $type, $icon)
+    {
+        parent::__construct($name, $price, $category, $type, $icon);
+    }
+}
