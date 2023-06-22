52efea19a8985f2a0701edf28061a59121722a33
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-06-22 12:21:12 +0200

    fix

--- models/bed.php
+++ models/bed.php
@@ -2,8 +2,8 @@
 
 class Bed extends Product
 {
-    public function __construct($name, $price, $category, $type, $icon)
+    public function __construct($name, $price, $category, $type, $icon, $img)
     {
-        parent::__construct($name, $price, $category, $type, $icon);
+        parent::__construct($name, $price, $category, $type, $icon, $img);
     }
 }
--- models/product.php
+++ models/product.php
@@ -8,14 +8,16 @@ class Product
     public $category;
     public $type;
     public $icon;
+    public $img;
 
-    public function __construct($name, $price, $category, $type, $icon)
+    public function __construct($name, $price, $category, $type, $icon, $img)
     {
         $this->name = $name;
         $this->price = $price;
         $this->category = $category;
         $this->type = $type;
         $this->icon = $icon;
+        $this->img = $img;
     }
 
     public function getName()
@@ -26,6 +28,11 @@ class Product
     public function getPrice()
     {
         return $this->price;
+    }
+
+    public function getImg()
+    {
+        return $this->img;
     }
 }
 
