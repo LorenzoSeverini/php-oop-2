85959af9de757f2e9205473a9a03934d283180f2
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-06-22 12:20:49 +0200

    add main js logic for purchase button

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// js 
+
+// function to complete the order
+function completeOrder() {
+    document.getElementById("completion-message").style.display = "block";
+}
+
+// function to delete the cart
+function deleteCart() {
+    document.getElementById("cart").style.display = "none";
+    // **********************
+    // **********************
+    // **********************
+}
